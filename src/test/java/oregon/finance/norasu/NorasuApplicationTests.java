a49c1be2ced2e900cb298218065532da8af6c17c
paulnora <zpaul9234@gmail.com>
2024-05-27 13:14:14 -0700

    init

---
+++ src/test/java/oregon/finance/norasu/NorasuApplicationTests.java
@@ -0,0 +1,13 @@
+package oregon.finance.norasu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NorasuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
